edc4509fc6a8c1e8014e433d1d1840f655b9f8a8
soon.good <playmymind@daum.net>
2021-08-08 14:04:03 +0900

    프로젝트 초기 구조 커밋

---
+++ example-codes/studyhazelcast-queue/src/main/java/io/study/studyhazelcastqueue/StudyhazelcastQueueApplication.java
@@ -0,0 +1,13 @@
+package io.study.studyhazelcastqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyhazelcastQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyhazelcastQueueApplication.class, args);
+	}
+
+}
---
+++ example-codes/studyhazelcast-queue/src/test/java/io/study/studyhazelcastqueue/StudyhazelcastQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package io.study.studyhazelcastqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyhazelcastQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
